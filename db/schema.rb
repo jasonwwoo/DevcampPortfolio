d591258feefc07821030d9473994f570bd05ece9
Jason Woo <jasonwwoo@gmail.com>
2018-06-29 12:56:28 -0500

    Tried to undo database and schema changes but failed.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_28_205159) do
+ActiveRecord::Schema.define(version: 2018_06_29_143047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_06_28_205159) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "status"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
