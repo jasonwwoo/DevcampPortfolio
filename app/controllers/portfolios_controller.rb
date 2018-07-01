ea5630219e8a388d0bee3c91b0aa8c408ebc4a81
Jason Woo <jasonwwoo@gmail.com>
2018-07-01 11:35:50 -0700

    implemented cutson scopes for angular and rubyonrails portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,6 +3,14 @@ class PortfoliosController < ApplicationController
 		@portfolio_items = Portfolio.all
 	end
 
+	def angular
+	  @portfolio_items = Portfolio.angular
+	end
+
+	def ruby_on_rails
+	  @portfolio_items = Portfolio.ruby_on_rails
+	end
+
 	def new
 		@portfolio_item = Portfolio.new
 	end
