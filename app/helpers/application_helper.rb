3981af408fafabf8c2a7f728a19ef0ec2817f603
Jason Woo <jasonwwoo@gmail.com>
2018-07-04 22:43:50 -0700

    integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
      end
   end
+
+  def copyright_generator
+    JasonViewTool::Renderer.copyright 'Jason Woo', "All rights reserved"
+  end
 end
